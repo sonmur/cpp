f4278ea79041ed37a5926a00b8239dd9dbe85a5c
sonmur <149419693+sonmur@users.noreply.github.com>
2024-05-15 01:15:00 +0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+ 
+int tabs = 0;
+ 
+struct Branch
+{
+ char Data;
+ Branch *LeftBranch;
+ Branch *RightBranch; 
+};
+ 
+ 
+void Add(char aData, Branch *&aBranch)
+{
+ if (!aBranch)
+ {
+ aBranch = new Branch;
+ aBranch->Data = aData;
+ aBranch->LeftBranch = 0;
+ aBranch->RightBranch = 0;
+ return;
+ }
+ else
+ if (aBranch->Data>aData)
+ {
+ Add(aData, aBranch->LeftBranch);
+ }
+ else
+ {
+ Add(aData, aBranch->RightBranch);
+ };
+}
+ 
+void print(Branch *aBranch)
+{
+ if (!aBranch) return;
+ tabs++;
+ 
+ print(aBranch->LeftBranch);
+ 
+ for (int i = 0; i<tabs; i++) cout << " ";
+ cout << aBranch->Data << endl;
+ 
+ 
+ print(aBranch->RightBranch);
+ 
+ tabs--;
+ return;
+}
+ 
+void FreeTree(Branch *aBranch)
+{
+ if (!aBranch) return;
+ FreeTree(aBranch->LeftBranch);
+ FreeTree(aBranch->RightBranch);
+ delete aBranch;
+ return;
+}
